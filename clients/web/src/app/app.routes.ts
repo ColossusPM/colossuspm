1ada3bcf469a5e004f19749f9863e296f9956a08
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2025-03-28 05:19:09 +0000

    Add Project feature with routing and Snowflake integration

--- clients/web/src/app/app.routes.ts
+++ clients/web/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 export const routes: Routes = [
   {
     path: '',
     title: 'Home - ColossusPM',
     component: IndexComponent,
+  },
+  {
+    path: 'projects/:id',
+    title: 'Project',
+    component: ProjectComponent,
   },
 ];
--- clients/web/src/app/pages/index/index.component.ts
+++ clients/web/src/app/pages/index/index.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Snowflake } from '@theinternetfolks/snowflake';
 
 @Component({
   selector: 'app-index',
   imports: [],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrl: './index.component.css',
 })
 export class IndexComponent {
+  constructor(private router: Router) {}
 
+  createProject() {
+    this.router.navigate(['/projects', Snowflake.generate()]);
+  }
 }
