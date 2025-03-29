5c0626f646943be7d5921ea9e1dfcbacdd3cd19a
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2025-03-29 09:54:26 +0530

    Remove create project component and integrate project creation into view projects component with modal dialog

--- clients/web/src/app/app.routes.ts
+++ clients/web/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
-import { CreateProjectComponent } from './pages/projects/create-project/create-project.component';
 import { ProjectSettingsComponent } from './pages/projects/project-settings/project-settings.component';
 import { ProjectViewComponent } from './pages/projects/project-view/project-view.component';
 import { ViewProjectsComponent } from './pages/view-projects/view-projects.component';
@@ -15,11 +14,6 @@ export const routes: Routes = [
     path: 'projects',
     title: 'Projects - ColossusPM',
     component: ViewProjectsComponent,
-  },
-  {
-    path: 'projects/create',
-    title: 'New Project - ColossusPM',
-    component: CreateProjectComponent,
   },
   {
     path: 'projects/:id/view',
--- clients/web/src/app/pages/view-projects/view-projects.component.ts
+++ clients/web/src/app/pages/view-projects/view-projects.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DatabaseService, Project } from '../../services/database.service';
 import { CommonModule } from '@angular/common';
-import { MessageModule } from 'primeng/message';
-import { CardModule } from 'primeng/card';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Snowflake } from '@theinternetfolks/snowflake';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { TextareaModule } from 'primeng/textarea';
+import { DatabaseService, Project } from '../../services/database.service';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ButtonModule, CardModule, CommonModule, MessageModule],
+  imports: [
+    ButtonModule,
+    CardModule,
+    CommonModule,
+    MessageModule,
+    DialogModule,
+    InputTextModule,
+    TextareaModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './view-projects.component.html',
   styleUrl: './view-projects.component.css',
 })
-export class ViewProjectsComponent {
+export class ViewProjectsComponent implements OnInit {
   projects: Project[] = [];
+  displayModal: boolean = false;
+
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl(''),
+  });
 
   constructor(
     private _router: Router,
@@ -25,8 +50,30 @@ export class ViewProjectsComponent {
     this.projects = await this._databaseService.getAllProjects();
   }
 
-  createProject() {
-    this._router.navigate(['/projects/create']);
+  showModal() {
+    this.displayModal = true;
+  }
+
+  hideModal() {
+    this.displayModal = false;
+    this.form.reset();
+  }
+
+  async create() {
+    if (this.form.invalid) return;
+
+    const projectId = Snowflake.generate();
+    await this._databaseService.createProject({
+      id: projectId,
+      name: this.form.value.name!,
+      description: this.form.value.description || '',
+      createdAt: new Date(),
+    });
+
+    this.displayModal = false;
+    this.form.reset();
+    this.projects = await this._databaseService.getAllProjects();
+    this._router.navigate(['/projects', projectId, 'view']);
   }
 
   openProject(project: Project) {
