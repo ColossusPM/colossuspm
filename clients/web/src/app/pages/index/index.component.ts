8f78c322dd411f6b5dbeaf8d8d29480d7b26232f
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2025-03-29 08:50:58 +0530

    Add footer component and update layout styles for improved structure

--- clients/web/src/app/pages/index/index.component.ts
+++ clients/web/src/app/pages/index/index.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [CommonModule, RouterModule, ButtonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
-
 }
